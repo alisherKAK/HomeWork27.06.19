4fa2be2a2300e61b54a1599a1c47d4d2550ae928
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-17 21:40:35 +0600

    Home Work 27.06.19

--- HomeWork27_06_19/MainWindow.xaml.cs
+++ HomeWork27_06_19/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace HomeWork27_06_19
 {
@@ -23,6 +27,41 @@ namespace HomeWork27_06_19
         public MainWindow()
         {
             InitializeComponent();
+
+            ParseRssFile();
+        }
+
+        private async void ParseRssFile()
+        {
+            string result;
+
+            using (var client = new WebClient())
+            {
+                result  = await client.DownloadStringTaskAsync(new Uri("http://www.playground.ru/rss/news.xml"));
+            }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(result);
+
+            XmlNodeList itemNodes = xmlDocument.SelectNodes("rss/channel/item");
+
+            foreach(XmlNode itemNode in itemNodes)
+            {
+                XmlNode rssSubNode = itemNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = itemNode.SelectSingleNode("guid");
+                string guid = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = itemNode.SelectSingleNode("comments");
+                string comments = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = itemNode.SelectSingleNode("link");
+                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = itemNode.SelectSingleNode("description");
+                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+            }
         }
     }
 }
